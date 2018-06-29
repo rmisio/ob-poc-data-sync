c4d0b8564014d42e36d2d9f0c881522a6aae43e5
Rob Misiorowski <rmisio@yahoo.com>
2018-06-29 10:20:48 -0700

    added link to the github repo

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <p style={{padding: '25px', textAlign: 'center'}}>🏠 is where the ❤️ is.</p>
+      <p style={{ textAlign: 'center'}}>🏠 is where the ❤️ is.</p>
     </div>
   );
 }
